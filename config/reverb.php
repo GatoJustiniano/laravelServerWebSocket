ca5406c6f2a21d111b625118b7b554a7793a50cc
GatoJustiniano <andres95liverpool@gmail.com>
2024-09-14 17:35:32 -0400

    prueba2

--- config/reverb.php
+++ config/reverb.php
@@ -40,10 +40,10 @@ return [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
                 'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
                 'server' => [
-                    'url' => env('REDIS_URL'),
+                    'url' => env('REDIS_URL', 'url'),
                     'host' => env('REDIS_HOST', '127.0.0.1'),
                     'port' => env('REDIS_PORT', '6379'),
-                    'username' => env('REDIS_USERNAME'),
+                    'username' => env('REDIS_USERNAME', 'userName'),
                     'password' => env('REDIS_PASSWORD'),
                     'database' => env('REDIS_DB', '0'),
                 ],
