812b2770d5979c476284d2a2733cf08db73928d8
GatoJustiniano <andres95liverpool@gmail.com>
2024-09-14 01:04:35 -0400

    seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Image;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Alvaro',
+            'email' => 'andres@gmail.com',
+            'password' => 'admin5095',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
